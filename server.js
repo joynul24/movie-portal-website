57989b648824727e6b8881acc46696d48fa1e53e
joynul24 <joynula919@gmail.com>
2026-06-09 12:09:35 +0600

    vercel.json file added

--- server.js
+++ server.js
@@ -189,9 +189,13 @@ async function startServer() {
     });
   }
 
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+  // app.listen(PORT, "0.0.0.0", () => {
+  //   console.log(`Server running on http://localhost:${PORT}`);
+  // });
 }
+
+app.get("/", (req, res) => {
+  res.send("Movie Portal server is running...");
+});
 
 startServer();
