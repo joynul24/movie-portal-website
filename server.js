cfbb46c379e2bee5a4c1374783ddb29ff1d383a6
joynul24 <joynula919@gmail.com>
2026-06-09 13:45:21 +0600

    firebase live url added in server

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "https://movie-prtal-website.web.app",
       "https://movie-portal-website.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
